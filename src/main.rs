d8f224f2039ed235f5ef73d0df1ca6360c9600ea
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-11-16 12:04:27 -0600

    Add basic commandline interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+#[macro_use]
+extern crate clap;
+use std::io;
+use std::io::Read;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap::App::new("stegosaurus")
+        .version(crate_version!())
+        .author(crate_authors!("\n"))
+        .about(crate_description!())
+        .arg(clap::Arg::from_usage("-f --file=<FILE> 'Sets the file to use as the base of the steganographic binary'"))
+        .get_matches();
+
+    if let Some(file) = matches.value_of("file") {
+        println!("Using file: {}", file);
+    }
+
+    let mut payload = Vec::new();
+    io::stdin().read_to_end(&mut payload)
+        .expect("Failed to read payload");
+
+    let payload = match String::from_utf8(payload) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("Invalid UTF-8 sequence: {}", e);
+            process::exit(1);
+        }
+    };
+
+    println!("Using payload: {}", payload);
 }
