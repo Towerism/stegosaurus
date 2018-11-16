7d95e3921bb5d8d01b4554d97de8c515ce417983
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-11-16 15:11:53 -0600

    Create basic interface for embedding data in bmp file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 extern crate stegosaurus;
 extern crate image;
 
+use std::error::Error;
 use std::process;
-use image::GenericImageView;
+use image::{
+    DynamicImage,
+    GenericImageView,
+    ImageBuffer,
+    RgbaImage
+};
 
 use stegosaurus::config::Config;
 
@@ -13,14 +19,69 @@ fn main() {
     });
     let Config { filename, payload } = config;
 
-    let payload = String::from_utf8(payload).unwrap();
 
-    let img = image::open(filename).unwrap_or_else(|err| {
+    let img = BmpBase::new(&filename).unwrap_or_else(|err| {
         eprintln!("stegosaurus: {}", err);
         process::exit(1);
     });
 
-    println!("Image dimensions: {:?}", img.dimensions());
+    let final_img = img.embed_data(&payload);
+    final_img.save("result.bmp").unwrap_or_else(|err| {
+        eprintln!("stegosaurus: {}", err);
+        process::exit(1);
+    });
 
-    println!("Using payload: {}", payload);
+    let payload = String::from_utf8(payload).unwrap();
+    println!("Used payload: {}", payload);
+
+
+}
+
+trait Embed {
+    fn embed_data(&self, data: &[u8]) -> Box<dyn Save>;
+}
+
+trait Save {
+    fn save(&self, path: &str) -> std::io::Result<()>;
+}
+
+struct BmpBase {
+    image: DynamicImage
+}
+
+impl BmpBase {
+    fn new(path: &str) -> Result<BmpBase, Box<dyn Error>> {
+        let image = image::open(path)?;
+
+        Ok(BmpBase {
+            image
+        })
+    }
+}
+
+struct BmpFinal {
+    buffer: RgbaImage
+}
+
+impl Embed for BmpBase {
+    fn embed_data(&self, data: &[u8]) -> Box<dyn Save> {
+        let (width, height) = self.image.dimensions();
+        let mut buffer = ImageBuffer::from_fn(width, height, |x, y| {
+            self.image.get_pixel(x, y)
+        });
+        for pixel in buffer.pixels_mut() {
+            for i in 0..4 {
+                pixel[i] &= 0b0010_0000;
+            }
+        }
+        Box::new(BmpFinal {
+            buffer
+        })
+    }
+}
+
+impl Save for BmpFinal {
+    fn save(&self, path: &str) -> std::io::Result<()> {
+        self.buffer.save(path)
+    }
 }
