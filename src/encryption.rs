6086d89f11c90788edc74554e76e5dd480284340
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-11-19 23:13:37 -0600

    Map openssl errors

--- src/encryption.rs
+++ src/encryption.rs
@@ -1,7 +1,6 @@
 use std::fmt;
 use std::error::Error;
 use openssl::symm;
-use openssl::hash;
 
 pub type InitializationVector = [u8; 16];
 type EncryptionKey = [u8; 32];
@@ -19,23 +18,24 @@ fn get_passphrase() -> Result<String, Box<dyn Error>> {
     let passphrase = read_passphrase_from_tty("passphrase: ")?;
     let confirm = read_passphrase_from_tty("confirm: ")?;
     if passphrase != confirm {
-        return Err(Box::new(PassphraseError { message: "Passphrases did not match" }));
+        return Err(Box::new(EncryptionError { message: "Passphrases did not match" }));
     }
     Ok(passphrase)
 }
 
-fn read_passphrase_from_tty(prompt: &str) -> Result<String, Box<PassphraseError>> {
+fn read_passphrase_from_tty(prompt: &str) -> Result<String, Box<EncryptionError>> {
     if let Ok(p) = rpassword::read_password_from_tty(Some(prompt)) {
         Ok(p)
     } else {
-        Err(Box::new(PassphraseError { message: "Could not read passphrase" }))
+        Err(Box::new(EncryptionError { message: "Could not read passphrase" }))
     }
 }
 
 fn derive_key(passphrase: &str, iv: &[u8]) -> Result<EncryptionKey, Box<dyn Error>> {
     let mut key = [0; 32];
     let pass = passphrase.as_bytes();
-    openssl::pkcs5::scrypt(&pass, &iv, 0x4000, 8, 1, 0x20000000, &mut key)?;
+    openssl::pkcs5::scrypt(&pass, &iv, 0x4000, 8, 1, 0x20000000, &mut key)
+        .map_err(|_| Box::new(EncryptionError { message: "failed to generate AES key" }))?;
     Ok(key)
 }
 
@@ -46,19 +46,20 @@ fn cipher() -> symm::Cipher {
 pub fn decrypt_payload(payload: &[u8], iv: &InitializationVector) -> Result<Vec<u8>, Box<dyn Error>> {
     let passphrase = read_passphrase_from_tty("passphrase: ")?;
     let key = derive_key(&passphrase, iv)?;
-    let decrypted = symm::decrypt(cipher(), &key, Some(iv), &payload)?;
+    let decrypted = symm::decrypt(cipher(), &key, Some(iv), &payload)
+        .map_err(|_| Box::new(EncryptionError { message: "failed to decrypt payload" }))?;
     Ok(decrypted)
 }
 
 #[derive(Debug)]
-pub struct PassphraseError {
+pub struct EncryptionError {
     message: &'static str
 }
 
-impl Error for PassphraseError {}
+impl Error for EncryptionError {}
 
-impl fmt::Display for PassphraseError {
+impl fmt::Display for EncryptionError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "passphrase error ({})", self.message)
+        write!(f, "encryption/decryption error ({})", self.message)
     } 
 }
